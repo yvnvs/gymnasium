b5f3fadbb0248713f9270292c25b1d7e332aed48
Yunus <yunusinne@gmail.com>
2023-02-24 18:09:40 +0300

    home page with animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/scenes/navbar";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
-
+import Home from "@/scenes/home"
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
@@ -28,7 +28,10 @@ function App() {
       <Navbar
         isTopOfPage={isTopOfPage}
        selectedPage={selectedPage}
-       setSelectedPage={setSelectedPage} />
+       setSelectedPage={setSelectedPage}
+      />
+      <Home        setSelectedPage={setSelectedPage}/>
+      
     </div>
   );
 }
